7a4e452f524bb901a990c17db2b4b15ff15f3ab6
Niraj Kumar <savealldocs@gmail.com>
2020-05-29 07:41:17 +1000

    working copy

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-import { encode, decode } from "./lib/nolib";
+const decode = (octal, conversionType) => {
+  // To represent the input as a 8-bytes array
+  var byteArray = [0, 0, 0, 0, 0, 0, 0, 0];
+  if (conversionType === 'hex') var long = parseInt(octal, 16);
+  else var long = parseInt(octal, 8);
+  for (var index = 0; index < byteArray.length; index++) {
+    var byte = long & 0xff;
+    byteArray[index] = byte;
+    long = (long - byte) / 256;
+  }
 
-const str = "31646541";
+  return byteArray;
+};
 
-const str2 = [103, 77, 97];
-const numberByteArray = decode(str, "octa");
-console.log(numberByteArray);
-const octalNumber = encode(numberByteArray, "octa");
-console.log(octalNumber);
+const encode = (byteArray, conversionType) => {
+  var value = 0;
+  for (var i = byteArray.length - 1; i >= 0; i--) {
+    value = value * 256 + byteArray[i];
+  }
+  if (conversionType === 'hex') return value.toString(16);
+  else return value.toString(8);
+};
+
+const str = '31646541';
+
+const numberByteArray = decode(str, 'octa');
+console.log('Decode- Octal Byte array', numberByteArray);
+const octalNumber = encode(numberByteArray, 'octa');
+console.log('Encode - Octal ', octalNumber);
+
+const hexaByteArray = decode(str, 'hex');
+console.log('Decode- Hexa Byte array', hexaByteArray);
+const hexaNumber = encode(hexaByteArray, 'hex');
+console.log('Encode - Hexa ', hexaNumber);
